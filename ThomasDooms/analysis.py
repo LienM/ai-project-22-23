4ec3bcd2feb2b6cf1dbb1bb7878358e203cd7575
thomas <thomas@dooms.eu>
2022-10-25 16:11:10 +0200

    deadline 2 #2

--- ThomasDooms/analysis.py
+++ ThomasDooms/analysis.py
@@ -3,16 +3,26 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+pd.options.display.max_columns = None
+pd.options.display.width = None
+pd.options.display.max_rows = None
+
 articles = pd.read_csv("data/articles.csv")
-customers = pd.read_csv("data/customers.csv")
-transactions = pd.read_csv("data/transactions.csv")
+# customers = pd.read_csv("data/customers.csv")
+# transactions = pd.read_csv("data/transactions.csv")
 
-# for col in ["graphical_appearance_no", "graphical_appearance_name", "perceived_colour_value_id"]:
-#     print("analyzing column", col, "on articles")
-#     print(articles[col].describe())
-#     print(set(articles[col]))
+# customers["single"] = customers.apply(lambda row: row["FN"] != 1.0 and row["Active"] == 1.0, axis=1)
+# print(len(customers[customers["single"]]))
 
-# for col in ["fashion_news_frequency", "FN", "Active"]:
+# print(articles.groupby(["index_code", "index_name"]).size())
+
+for col in articles.columns:
+    if col in ["article_id", "product_code", "prod_name", "detail_desc"]: continue
+    print("analyzing column", col, "on articles")
+    print(articles[col].describe())
+    print(set(articles[col].fillna("")))
+
+# for col in ["club_member_status", "FN", "Active"]:
 #     print("analyzing column", col, "on customers")
 #     print(customers[col].describe())
 #     print(set(customers[col].fillna("")))
@@ -25,9 +35,7 @@ transactions = pd.read_csv("data/transactions.csv")
 # plt.show()
 
 # sns.set_palette(palette)
-pd.options.display.max_columns = None
-pd.options.display.width = None
-pd.options.display.max_rows = None
+
 
 # plot the distribution of age and amount of transactions
 # merged = transactions.merge(customers, on="customer_id", how="left")
@@ -49,21 +57,21 @@ pd.options.display.max_rows = None
 # customers[["customer_id", "prediction"]].to_csv("submission.csv", index=False)
 
 # plot the price per article by merging the two datasets
-merged = transactions.merge(articles, on="article_id", how="left")
-summed = merged.groupby(by="article_id")["price"].mean().reset_index(name="mean")
-
-print(summed["mean"].describe())
+# merged = transactions.merge(articles, on="article_id", how="left")
+# summed = merged.groupby(by="article_id")["price"].mean().reset_index(name="mean")
+#
+# print(summed["mean"].describe())
 
 # sns.displot(summed, x="mean")
 # plt.show()
 
-print('\n\n\n\n')
+# print('\n\n\n\n')
 
 # plot the mean of purchase price per customer by merging the two datasets
-merged = transactions.merge(customers, on="customer_id", how="left")
-summed = merged.groupby(by="customer_id")["price"].mean().reset_index(name="mean")
-
-print(summed["mean"].describe())
+# merged = transactions.merge(customers, on="customer_id", how="left")
+# summed = merged.groupby(by="customer_id")["price"].mean().reset_index(name="mean")
+#
+# print(summed["mean"].describe())
 
 # sns.displot(summed, x="mean")
 # plt.show()

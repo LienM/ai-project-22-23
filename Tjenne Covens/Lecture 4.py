5e48092165c6cf3c03e2f446b7518b2f8d1e4fe0
TjenCov <treapy2.0@gmail.com>
2022-11-27 10:26:07 +0100

    :o)

--- Tjenne Covens/Lecture 4.py
+++ Tjenne Covens/Lecture 4.py
@@ -2,9 +2,13 @@ import numpy as np
 import pandas as pd
 import random
 from scipy import spatial
-from LabelEncoder import MultiLabelEncoder
 import lightgbm as lgb
-import csv
+
+from ast import literal_eval
+
+from LabelEncoder import MultiLabelEncoder
+from utils import map_at_12, extract_last_week_sales, extract_all_but_last_week_sales
+from Parameters import PARAM
 
 def warn(*args, **kwargs):
     pass
@@ -17,7 +21,6 @@ from sklearn.cluster import KMeans
 
 
 multi_encoder = MultiLabelEncoder()
-WRITE = True
 
 
 def load_data():
@@ -28,8 +31,7 @@ def load_data():
     print("loading transactions ...")
     transactions = pd.read_csv('../../transactions_sample1.csv.gz')
     print("creating samples ...")
-    samples = create_sample(transactions, articles, customers)
-    return transactions, articles, customers, samples
+    return transactions, articles, customers
 
 
 def create_sample(transactions, articles, customers):
@@ -76,7 +78,7 @@ def create_sample(transactions, articles, customers):
 
 def preprocess_transactions(transactions):
     # I'm dropping a lot of columns, use them in your engineering tasks!
-    transactions_processed = transactions[['customer_id', 'article_id', 'sales_channel_id', 'price']].copy()
+    transactions_processed = transactions[PARAM["transaction"]].copy()
     transactions_processed.head()
 
     transactions_processed = transactions_processed.fillna(0)
@@ -91,8 +93,7 @@ def preprocess_transactions(transactions):
 
 
 def preprocess_articles(articles):
-    articles_processed = articles[
-        ["article_id", "colour_group_code", "department_no", "index_group_no", "section_no", "garment_group_no"]].copy()
+    articles_processed = articles[PARAM["article"]].copy()
     articles_processed = multi_encoder.encode("article_id", "article_id", articles_processed)
     articles_processed = articles_processed.fillna(0)
 
@@ -100,7 +101,7 @@ def preprocess_articles(articles):
 
 
 def preprocess_customers(customers):
-    customers_processed = customers[["customer_id", "age", "postal_code"]].copy()
+    customers_processed = customers[PARAM["customer"]].copy()
     multi_encoder.create_encoder("postal_code")
     customers_processed = multi_encoder.encode("customer_id", "customer_id", customers_processed)
     customers_processed = multi_encoder.encode("postal_code", "postal_code", customers_processed)
@@ -110,11 +111,11 @@ def preprocess_customers(customers):
 
 
 def preprocess_data():
-    transactions, articles, customers, samples = load_data()
-
+    transactions, articles, customers = load_data()
     transactions = preprocess_transactions(transactions)
     articles = preprocess_articles(articles)
     customers = preprocess_customers(customers)
+    samples = create_sample(transactions, articles, customers)
 
     return transactions, articles, customers, samples
 
@@ -132,50 +133,58 @@ def recent_sales_k_months(transactions, articles, k, candidates=None):
     # Getting all sold items within the last 6 months
     recent_items = dated_transactions.loc[dated_transactions["transaction_age_months"] < k]
     recent_items = recent_items["article_id"].value_counts().rename_axis("article_id").reset_index(name="count")
+    recent_items = recent_items.sort_values(by="count", ascending=False)["article_id"].head(100)
+    # recent_items = recent_items.merge(articles, how="inner", on="article_id")
 
-    recent_items = recent_items.merge(articles, how="inner", on="article_id")
-    # # Setting up dict with all candidates for
-    # for customer_id in customers["customer_id"].values:
-    #     candidates[customer_id] = recent_items["article_id"].head(100)
-    return recent_items
+    return list(recent_items)
 
 
-def candidates_selection(transactions, articles, customers, samples):
-    # recent_items = recent_sales_k_months(samples, articles, 6)
+def candidates_selection(transactions, articles, customers, samples, candidates=None):
+    recent_items = recent_sales_k_months(samples, articles, 2)
     print("starting candidate selection")
-    candidates = k_kk_clustering(transactions, articles, customers)
-    if WRITE:
+    candidates = k_nn_clustering(transactions, recent_items, customers, candidates)
+    if PARAM["SAVE"]:
         save_candidates(candidates)
-    # candidates = rank_candidates(candidates, articles, samples)
     return candidates
 
 
-def k_kk_clustering(transactions, articles, customers, candidates=None):
+def k_nn_clustering(transactions, articles, customers, candidates=None, k=13600):
     # prepare transaction data
+    transactions.drop(columns=["t_dat", "ordered"], inplace=True)
     sales = transactions.merge(articles, how="inner", on='article_id')
+    sales.drop(columns=["price", "sales_channel_id"], inplace=True)
+
     # prepare candidates dict
     if candidates is None:
-        candidates = {}
+        candidates = {"customers": [], "sims": []}
     c = customers["customer_id"].values
-    for customer in c:
+    i = k
+    for customer in range(k, len(c)):
+        if i % 100 == 0:
+            if i != k:
+                print(f"{i}/{len(c)}")
+                save_candidates(candidates)
+                candidates = {"customers": [], "sims": []}
         # check if customer is already present in candidates dict
-        if customer not in candidates:
-            candidates[customer] = []
-        similarities = pd.DataFrame(columns=['article_id', 'sim'])
+        # if customer not in candidates:
+        #     candidates[customer] = []
+        # similarities = pd.DataFrame(columns=['article_id', 'sim'])
+        similarities = {}
 
         # filter purchases of given customer
-        purchases = sales.loc[sales["customer_id"] == customer].copy()
-        purchased_articles = purchases["article_id"].values
-        purchases.drop(columns=["customer_id", "article_id", "sales_channel_id", "price"], inplace=True)
+        purchases = sales[sales["customer_id"] == c[customer]].copy()
+        # purchased_articles = purchases["article_id"].values
 
         # skip if customer has not made purchases yet
         if purchases.shape[0] == 0:
             continue
-        k = 6
-        if purchases.shape[0] <= 12:
-            k = 2
-            if purchases.shape[0] == 1:
-                k = 1
+
+        purchases = purchases.drop(columns=["customer_id", "article_id"])
+
+
+        k = purchases.shape[0] // 15
+        if k == 0:
+            k = 1
 
         # find clusters and calculate centroids
         clusters = KMeans(n_clusters=k, init="k-means++", random_state=0).fit(purchases)
@@ -184,51 +193,135 @@ def k_kk_clustering(transactions, articles, customers, candidates=None):
         # find K nearest neighbourgs
         for index, article in articles.iterrows():
             article_id = int(article["article_id"])
-            if article_id in purchased_articles:
-                continue
-            value_list = article.values.flatten().tolist()
+            # if article_id in purchased_articles:
+            #     continue
+            value_list = article.values.tolist()
             value_list.pop(0)
+
             # calculate similarities between clusters and item
             top_similarity = 0
             for centroid in centroids:
                 similarity = 1 - spatial.distance.cosine(value_list, centroid)
+
                 # if similarity with a cluster is high enough, item gets added to candidates
                 if similarity >= top_similarity:
                     top_similarity = similarity
-            similarities.loc[len(similarities.index)] = [article_id, top_similarity]
-        similarities = similarities.sort_values(by=["sim"], ascending=False)
-        candidates[customer] = similarities["article_id"].head(200)
+            similarities[index] = {"article_id": article_id, "sim": top_similarity}
 
+        sim_df = pd.DataFrame.from_dict(similarities, "index")
+        sim_df = sim_df.sort_values(by=["sim"], ascending=False)
+        candidates["customers"].append(c[customer])
+        candidates["sims"].append(sim_df["article_id"].head(200).values.tolist())
+        i += 1
     return candidates
 
 
 def rank_candidates(candidates, articles, samples):
-    # setting up data
-    X_train, X_test, y_train, y_test = train_test_split(samples.drop('ordered', axis=1),
-                                                        samples['ordered'], test_size=0.10,
-                                                        random_state=42)
-    training_data = lgb.Dataset(X_train, label=y_train)
-    validation_data = lgb.Dataset(X_test, label=y_test)
+    samples = samples[PARAM["samples"]].copy()
+    samples["customer_id"] = pd.to_numeric(samples["customer_id"])
+    samples["article_id"] = pd.to_numeric(samples["article_id"])
+    samples["ordered"] = pd.to_numeric(samples["ordered"])
 
-    # setting up training parameters
-    param = {'num_leaves': 31, 'objective': 'binary', 'metric': 'auc'}
-    num_round = 10
-    bst = lgb.train(param, training_data, num_round, valid_sets=[validation_data])
-    for customer in candidates.keys():
-        candidate_articles = articles.loc[articles['article_id'].isin(candidates[customer])].copy()
+    # setting up data
+    training_data = extract_all_but_last_week_sales(samples, False)
+    validation_data = extract_last_week_sales(samples)
+
+    qids_train = training_data.groupby(["customer_id"])["article_id"].count().to_numpy()
+    qids_validation = validation_data.groupby(["customer_id"])["article_id"].count().to_numpy()
+
+    training_data = training_data.drop(["t_dat"], axis=1)
+    x_train = training_data.drop(columns=["ordered"])
+    y_train = training_data["ordered"]
+
+    validation_data = validation_data.drop(["t_dat"], axis=1)
+    x_test = validation_data.drop(columns=["ordered"])
+    y_test = validation_data["ordered"]
+
+    ranker = lgb.LGBMRanker(
+        objective="lambdarank",
+        metric="auc",
+        boosting_type="dart",
+        n_estimators=100,
+        importance_type='gain',
+        verbose=10,
+        min_child_samples=1,
+        num_leaves=50
+    )
+
+    ranker.fit(
+        X=x_train,
+        y=y_train,
+        group=qids_train,
+        eval_set=[(x_test, y_test)],
+        eval_group=[qids_validation],
+        eval_at=10,
+    )
+
+    candidates["prediction"] = []
+    for customer in range(len(candidates["customers"])):
+        candidate_articles = articles.loc[articles['article_id'].isin(candidates["sims"][customer])].copy()
+        candidate_articles['customer_id'] = candidates["customers"][customer]
         if candidate_articles.shape[0] > 0:
-            predictions = bst.predict(candidate_articles)
+            candidate_articles["prediction"] = ranker.predict(candidate_articles)
             candidate_articles = candidate_articles.sort_values(by=["prediction"], ascending=False)
-            candidates[customer] = candidate_articles.head(12)
+            candidates["prediction"].append(list(candidate_articles["article_id"].head(12)))
+
+    return candidates
 
 
 def save_candidates(candidates):
-    with open('candidates.csv', 'w', newline='') as f:
-        writer = csv.DictWriter(f, fieldnames=candidates.keys())
-        writer.writeheader()
-        writer.writerow(candidates)
+    df = pd.DataFrame(candidates)
+    df.to_csv('candidates.csv', mode='a', index=False, header=False)
+
+
+def load_candidates(file="candidates.csv"):
+    candidates = pd.read_csv(file)
+    candidates["sims"] = candidates["sims"].apply(literal_eval)
+    candidates = candidates.to_dict(orient='list')
+    return candidates
+
+
+def create_submission(candidates):
+    # create dataframe
+    submission = pd.DataFrame(columns=["customer_id", "prediction"])
+    submission["customer_id"] = candidates["customers"]
+    # submission["prediction"] = candidates["prediction"]
+
+    # label decode
+    submission = multi_encoder.decode("customer_id", "customer_id", submission)
+    submission = submission.reset_index()
+    for prediction in range(len(candidates["prediction"])):
+        dataframe = pd.DataFrame(columns=["article_id"])
+        dataframe["article_id"] = candidates["prediction"][prediction]
+        dataframe = multi_encoder.decode("article_id", "article_id", dataframe)
+        submission.at[prediction, 'prediction'] = list(dataframe["article_id"])
+
+    submission.drop(columns=["index"], inplace=True)
+    submission.to_csv('submission.csv', index=False, header=True)
+
 
 if __name__ == "__main__":
+    # load dataset
     transactions, articles, customers, samples = preprocess_data()
-    candidates = candidates_selection(transactions, articles, customers, samples)
+
+    candidates = {}
+    # run candidate selection
+    if PARAM["SELECT"]:
+        candidates = candidates_selection(transactions, articles, customers, samples)
+
+    # load candidates from file
+    if PARAM["LOAD"]:
+        candidates = load_candidates()
+
+    # rank candidates and calculate precision
+    # popular_items = recent_sales_k_months(transactions, articles, 6)
+    # for customer in range(len(candidates["customers"])):
+    #     candidates["sims"][customer] += popular_items
+
+    candidates = rank_candidates(candidates, articles, samples)
+    map_at_12(samples, candidates)
+
+    # write submission
+    if PARAM["SUBMIT"]:
+        create_submission(candidates)
 
---
+++ Tjenne Covens/Parameters.py
@@ -0,0 +1,13 @@
+
+PARAM = {
+    "article": ["article_id", "colour_group_code", "department_no", "index_group_no", "section_no", "garment_group_no"],
+    "customer": ["customer_id", "age", "postal_code"],
+    "transaction": ['customer_id', 'article_id', 'sales_channel_id', 'price', 't_dat'],
+    "samples": ["customer_id", "ordered", "article_id", "colour_group_code", "department_no", "index_group_no", "section_no", "garment_group_no", "t_dat"],
+    "SELECT": False,
+    "SUBMIT": False,
+    "LOAD": True,
+    "SAVE": True
+}
+
+
